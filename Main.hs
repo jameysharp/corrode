a17dbb510c15da46acc473927d96f68173d29584
Jamey Sharp <jamey@minilop.net>
2016-05-29 20:35:55 -0700

    Transform return statements to idiomatic Rust.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import Control.Monad
 import Language.C
 import Language.C.System.GCC
 import Language.Rust.Corrode.C
+import Language.Rust.Idiomatic
 import System.Environment
 import Text.PrettyPrint.HughesPJClass
 
@@ -11,5 +12,5 @@ main = do
     forM_ args $ \ arg -> do
         Right (CTranslUnit decls _node) <- parseCFile (newGCC "gcc") Nothing [] arg
         forM_ decls $ \ decl -> case decl of
-            CFDefExt f -> putStrLn (prettyShow (interpretFunction f))
+            CFDefExt f -> putStrLn (prettyShow (itemIdioms (interpretFunction f)))
             _ -> print decl
---
+++ src/Language/Rust/Idiomatic.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Language.Rust.Idiomatic (
+    itemIdioms
+) where
+
+import qualified Language.Rust.AST as Rust
+
+unsnoc :: [a] -> Maybe ([a], a)
+unsnoc [] = Nothing
+unsnoc [x] = Just ([], x)
+unsnoc (x:xs) = Just (x:a, b)
+    where Just (a,b) = unsnoc xs
+
+tailExpr :: Rust.Expr -> Maybe (Maybe Rust.Expr)
+-- If the last statement in this block is a return statement, extract
+-- its expression (if any) to the final expression position.
+tailExpr (Rust.Return e) = Just e
+-- If the last statement is a block, that's a tail-block.
+tailExpr (Rust.BlockExpr b) = Just (Just (Rust.BlockExpr (tailBlock b)))
+-- If the last statement is an if-expression, its true and false blocks
+-- are themselves tail-blocks.
+-- TODO: treat match-expressions like if-expressions.
+tailExpr (Rust.IfThenElse c t f) = Just (Just (Rust.IfThenElse c (tailBlock t) (tailBlock f)))
+-- Otherwise, there's nothing to rewrite.
+tailExpr _ = Nothing
+
+-- Eliminate any return statement that has no statements which
+-- dynamically follow it.
+tailBlock :: Rust.Block -> Rust.Block
+-- If this block already has a final expression, just try to get rid of
+-- return statements within that expression.
+tailBlock (Rust.Block b (Just (tailExpr -> Just e))) = Rust.Block b e
+-- If there's no final expression but the final statement consists of an
+-- expression that makes sense in the tail position, move it.
+tailBlock (Rust.Block (unsnoc -> Just (b, Rust.Stmt (tailExpr -> Just e))) Nothing) = Rust.Block b e
+-- Otherwise, leave this block unchanged.
+tailBlock b = b
+
+itemIdioms :: Rust.Item -> Rust.Item
+itemIdioms (Rust.Function name formals ret b) = Rust.Function name formals ret (tailBlock b)
