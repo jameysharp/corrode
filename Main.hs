6d03b33caadc60baf553201222e684565823e47d
Jamey Sharp <jamey@minilop.net>
2016-07-05 20:24:19 -0700

    Report preprocessor errors better.
    They should go to stderr and result in a failure exit status.

--- Main.hs
+++ Main.hs
@@ -3,6 +3,8 @@ import Language.C.System.GCC
 import Language.Rust.Corrode.C
 import Language.Rust.Idiomatic
 import System.Environment
+import System.Exit
+import System.IO
 import Text.PrettyPrint.HughesPJClass
 
 main :: IO ()
@@ -10,5 +12,7 @@ main = do
     file : options <- getArgs
     parsed <- parseCFile (newGCC "gcc") Nothing options file
     case parsed of
-        Left err -> print err
+        Left err -> do
+            hPrint stderr err
+            exitFailure
         Right t -> mapM_ (putStrLn . prettyShow . itemIdioms) (interpretTranslationUnit t)
